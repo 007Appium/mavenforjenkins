19ebd487e0f8d368fd2ef2ee8640f9dd46349dcc
007Appium <111ankitsingh@gmail.com>
2021-05-23 17:29:59 +0530

    paramater

--- src/test/java/unitTest/FirstProgram.java
+++ src/test/java/unitTest/FirstProgram.java
@@ -5,8 +5,11 @@ import java.util.concurrent.TimeUnit;
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.edge.EdgeDriver;
+import org.openqa.selenium.firefox.FirefoxDriver;
 import org.testng.Assert;
 import org.testng.annotations.BeforeMethod;
+import org.testng.annotations.Parameters;
 import org.testng.annotations.Test;
 
 import io.github.bonigarcia.wdm.WebDriverManager;
@@ -19,11 +22,18 @@ public class FirstProgram {
 	WebDriver driver;
 	String url = "https://opensource-demo.orangehrmlive.com/";
 	
-	
+	@Parameters("Browser")
 	@BeforeMethod
-	public void beforeMethod(){
-		WebDriverManager.chromedriver().setup();
-		driver= new ChromeDriver();
+	public void beforeMethod(String browserName){
+		System.out.println("Paramater value is : "+browserName );
+		if (browserName.contains("chrome")) {
+			WebDriverManager.chromedriver().setup();
+			driver= new ChromeDriver();
+		}else{
+			WebDriverManager.firefoxdriver().setup();
+			driver= new FirefoxDriver();
+		}
+		
 		driver.manage().window().maximize();
 		wait.implicitWait(driver,10);
 	}
